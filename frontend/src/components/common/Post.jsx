2567a10c715b113170195c808ec354ef401cd778
Akash <117592488+akashbhatc@users.noreply.github.com>
2024-07-16 13:56:49 +0530

    Update Post.jsx

--- frontend/src/components/common/Post.jsx
+++ frontend/src/components/common/Post.jsx
@@ -159,7 +159,7 @@ const Post = ({ post }) => {
             {post.img && (
               <img
                 src={post.img}
-                className="h-80 object-contain rounded-lg border border-gray-700"
+                className="h-100 object-contain rounded-lg border border-gray-700"
                 alt=""
               />
             )}

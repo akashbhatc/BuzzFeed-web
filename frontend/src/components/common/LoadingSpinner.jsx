c31ffdc0d60ccbbe4610df1ec850a80c44826633
Akash <117592488+akashbhatc@users.noreply.github.com>
2024-07-16 16:13:45 +0530

    Update LoadingSpinner.jsx

--- frontend/src/components/common/LoadingSpinner.jsx
+++ frontend/src/components/common/LoadingSpinner.jsx
@@ -1,7 +1,5 @@
-const LoadingSpinner = () => {
-  
-	return <span className="loading loading-spinner loading-lg text-info"></span>;
-  };
-  
-  export default LoadingSpinner;
-  
+const LoadingSpinner = ({size}) => {
+  return <span className={`loading loading-spinner loading-${size} text-info`}></span>;
+};
+
+export default LoadingSpinner;
